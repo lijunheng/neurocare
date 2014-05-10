b2fbdeb902c556597653cd48d36339b3d2f5b7be
Frank <frank.junheng@gmail.com>
2014-05-10 12:40:49 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neurocare::Application.config.secret_key_base = '2d3adb117e3b8090b3b9a08b8490bfa27eac2ce63ed120aa4b4fa7904f18870819ed74fd4cb9baeb09027a3ec7587318ada4ecf70a83f9370f660f19289eaf6f'
