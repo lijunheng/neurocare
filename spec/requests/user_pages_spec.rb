23aeb5df15e31ca269b8f39e8aa6648d2e1be5e7
Frank <frank.junheng@gmail.com>
2014-05-12 21:30:52 -0400

    Edited Users index

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -81,6 +81,19 @@ describe "UserPages" do
 
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
+    end
+    
     before do
       sign_in user
       visit edit_user_path(user)
