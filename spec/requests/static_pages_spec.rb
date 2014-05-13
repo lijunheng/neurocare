5b46370b1f2f6d3f9f676e861ced1dd7c5b91d35
Frank <frank.junheng@gmail.com>
2014-05-12 22:51:11 -0400

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,20 @@ describe "StaticPages" do
     before { visit root_path }
     it { should have_content('Duke Neurocare') }
     it { should have_title(full_title('')) }
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
   describe "Events page" do
     before { visit events_path }
